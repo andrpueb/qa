5783939188ba28982a10d7e475ad00be1e062124
Andres <andres@mightyhive.com>
2021-04-22 12:18:07 +0100

    datalayer - reading, checking and writing done

--- check_pages.js
+++ check_pages.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const qa_settings = require('./qa_settings')
+const qasettings = require('./qa_settings')
 
 
 //make it node crash on error
@@ -16,12 +16,14 @@ const page_url = 'https://fightingillini.com/';
 
 //(async () => {
 async function check_pages(url_list) {
-  //console.log(url_list)
-  for (const url of url_list) {
-    const clean_url = url.includes('https//') ? url : 'https://' + url;
-    const browser = await puppeteer.launch({
+  const browser = await puppeteer.launch({
       headless: true
     });
+
+  let response_data = []
+
+  for (const url of url_list) {
+    const clean_url = url.includes('https//') ? url : 'https://' + url;
     const page = await browser.newPage();
 
     const client = await page.target().createCDPSession();
@@ -51,26 +53,26 @@ async function check_pages(url_list) {
 
     const check_parameters = await page.evaluate(() => {
       const is_datalayer = window.dataLayer ? true : false;
-      console.log('test')
-      const all_params = dataLayer.find(({
-        event
-      }) => event === 'dataLayerLoaded');
+      const all_params = is_datalayer ? dataLayer.find(({event}) => event === 'dataLayerLoaded'): false;
+      let school_params = [];
+      let property_id, dl_defined, ssClientID;
       if (all_params) {
-        const school_ids = {
-          'isdatalayer': is_datalayer,
-          'gaPropertyID': all_params.gaPropertyID,
-          'ssClientID': all_params.schoolAttributes.ssClientID
-        }
-        return school_ids
+        property_id = all_params.gaPropertyID;
+        dl_defined = is_datalayer;
+        ssClientID = all_params.schoolAttributes.ssClientID;
+        school_params.push(property_id, dl_defined, ssClientID)
+      }else{
+        school_params.push('undefined', is_datalayer, 'undefined')
       }
+      return school_params;;
     })
-    console.log(check_parameters)
+    response_data.push(check_parameters)
+    console.log(response_data)
     //console.log(results)
     page.close()
   };
+  qasettings.gswrite(response_data)
+  await browser.close();
 };
 
-module.exports = {
-  check_pages
-}
-//})();
+module.exports.check_pages = check_pages
--- qa_settings.js
+++ qa_settings.js
@@ -3,52 +3,62 @@ const creds = require('./credentials.json');
 
 const run_check = require('./check_pages')
 
-
   const client = new google.auth.JWT(
     creds.client_email,
     null,
     creds.private_key,
     ['https://www.googleapis.com/auth/spreadsheets']
   );
-  const options = {
+  const read_options = {
     spreadsheetId: '1n6wZmWrvuGD-Va3dgY7E9mUWF72KbbtT04pMU9oUBR8',
     range: 'A4:A13'
   };
 
-  client.authorize(function (err, tokens) {
+  let client_auth;
+    client.authorize(function (err, tokens) {
       if (err) {
         console.log(err);
         return;
       } else {
-        console.log('Connected');
-        gsrun(client)
+        console.log('connected')
+        client_auth = client;
+        gsread(client_auth)
       }
   });
 
 
-  async function gsrun(cli) {
+  async function gsread(cli) {
     const gsapi = google.sheets({
       version: 'v4',
       auth: cli
     })
 
-    let data = await gsapi.spreadsheets.values.get(options);
+    let data = await gsapi.spreadsheets.values.get(read_options);
 
     let url_list = data.data.values;
-    //console.log(url_list)
     let array_list = []
     url_list.forEach((url) => {
       array_list.push(url[0])
     })
-
-    //console.log(array_list)
-    run_check.check_pages(array_list)
+    run_check.check_pages(array_list, client)
   }
 
-  // (async() =>{
-  //   console.log('before start')
-  //   await auth();
-  //   console.log('after start')
-  // })();
+  async function gswrite(pages_data){
+    const gsapi = google.sheets({
+      version: 'v4',
+      auth: client_auth
+    })
+    const write_options = {
+      spreadsheetId: '1n6wZmWrvuGD-Va3dgY7E9mUWF72KbbtT04pMU9oUBR8',
+      range: 'J4',
+      valueInputOption: 'USER_ENTERED',
+      resource :{values: pages_data}
+    };
 
-// module.exports = {auth}
+    let write_spreadsheet = gsapi.spreadsheets.values.update(write_options)
+     console.log(write_spreadsheet)
+
+  }
+
+
+  module.exports.gswrite = gswrite
